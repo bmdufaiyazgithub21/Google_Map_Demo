bf770dc15ea65a4253237619b2430df8edae12f3
Faiyaz Ahmad <bmdufaiyaz@gmail.com>
2024-04-03 12:22:04 +0530

    7th commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,79 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  @override
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Google Maps Demo',
-      home: MapScreen(),
+      home: MyApp(),
     );
   }
-}
 
-class MapScreen extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _MapScreenState createState() => _MapScreenState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MapScreenState extends State<MapScreen> {
+class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
+  static final LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+  LatLng _currentMapPosition = _center;
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_currentMapPosition.toString()),
+        position: _currentMapPosition,
+        infoWindow: InfoWindow(
+            title: 'Nice Place',
+            snippet: 'Welcome to Poland'
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _currentMapPosition = position.target;
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Maps Demo'),
-      ),
-      body: GoogleMap(
-        onMapCreated: (GoogleMapController controller) {
-          mapController = controller;
-        },
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.422, -122.084),
-          zoom: 10,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Maps Demo'),
+          backgroundColor: Colors.green,
+        ),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 10.0,
+                ),
+                markers: _markers,
+                onCameraMove: _onCameraMove
+            ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  onPressed: _onAddMarkerButtonPressed,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.map, size: 30.0),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
